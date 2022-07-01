eeb8ea558470ead5d09577e85a3f50c14ad04e29
derekchancts <58282201+derekchancts@users.noreply.github.com>
2022-07-02 05:31:36 +0800

    changed PORT

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const schema = makeExecutableSchema({ typeDefs, resolvers });
 // }
 
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 
 // const server = new ApolloServer({
